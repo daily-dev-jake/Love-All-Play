4ca61784902ebe4959440375875a96ee380f3444
daily-dev-jake <jlele92@outlook.com>
2022-10-15 00:34:31 +0800

    Made styles for selected nav links

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,32 +1,61 @@
 import React from "react";
-import './BottomNav.css'
+import { NavLink } from "react-router-dom";
+
+import "./BottomNav.css";
 import { HiHome } from "react-icons/hi";
 import { HiOutlineHome } from "react-icons/hi";
 import { MdDescription } from "react-icons/md";
 import { MdOutlineDescription } from "react-icons/md";
-import { IoMdSettings } from "react-icons/io5";
+import { IoMdSettings } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUserCircle } from "react-icons/fa";
 import { FaRegUserCircle } from "react-icons/fa";
 
 const BottomNav = () => {
   return (
-    <footer className='Btm-nav'>
-      {/* <div className='Btm-wrapper'> */}
-        <div className='Home-nav'>
-          <HiHome fontSize={52} />
-        </div>
-        <div className='Log-nav'>
-          <MdOutlineDescription fontSize={50} />
-        </div>
-        <div className='Settings-nav'>
-          <IoSettingsOutline fontSize={50} />
-        </div>
-        <div className='Profile-nav'>
-          <FaRegUserCircle fontSize={46} />
-        </div>
-      {/* </div> */}
-    </footer>
+    <nav className='Btm-nav'>
+      <NavLink
+        to='/'
+        exact
+        className={({ isActive }) => (isActive ? "active" : "inactive")}>
+        {({ isActive }) =>
+          isActive ? <HiHome fontSize={65} /> : <HiOutlineHome fontSize={52} />
+        }
+      </NavLink>
+      <NavLink
+        to='/records'
+        className={({ isActive }) => (isActive ? "active" : "inactive")}>
+        {({ isActive }) =>
+          isActive ? (
+            <MdDescription fontSize={65} />
+          ) : (
+            <MdOutlineDescription fontSize={50} />
+          )
+        }
+      </NavLink>
+      <NavLink
+        to='/settings'
+        className={({ isActive }) => (isActive ? "active" : "inactive")}>
+        {({ isActive }) =>
+          isActive ? (
+            <IoMdSettings fontSize={65} />
+          ) : (
+            <IoSettingsOutline fontSize={50} />
+          )
+        }
+      </NavLink>
+      <NavLink
+        to='/user'
+        className={({ isActive }) => (isActive ? "active" : "inactive")}>
+        {({ isActive }) =>
+          isActive ? (
+            <FaUserCircle fontSize={65} />
+          ) : (
+            <FaRegUserCircle fontSize={48} />
+          )
+        }
+      </NavLink>
+    </nav>
   );
 };
 
