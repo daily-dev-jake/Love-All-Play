82c556ad83f487414c971bb4cd37dc7c459c4460
daily-dev-jake <jlele92@outlook.com>
2023-04-19 21:16:13 +0800

    added working matches into GamePage

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -7,11 +7,20 @@ import "./GamePage.css";
 const DEBUG = true;
 
 const debugScores = {
+  player1: 19,
+  player2: 19,
+};
+// const debugMatchScores = {
+//   player1: 0,
+//   player2: 0,
+// };
+
+const playerScores = {
   player1: 0,
   player2: 0,
 };
 
-const playerScores = {
+const playerMatchScores = {
   player1: 0,
   player2: 0,
 };
@@ -21,6 +30,12 @@ const GamePage = () => {
   const [topCourtScore, setTopCourtScore] = useState(playerScores.player1);
   const [btmCourtScore, setBtmCourtScore] = useState(playerScores.player2);
 
+  const [player1MatchScore, setplayer1MatchScore] = useState(
+    playerMatchScores.player1
+  );
+  const [player2MatchScore, setplayer2MatchScore] = useState(
+    playerMatchScores.player2
+  );
   useEffect(() => {
     if (DEBUG) {
       //initial states
@@ -28,7 +43,12 @@ const GamePage = () => {
       setBtmCourtScore(debugScores.player2);
       playerScores.player1 = debugScores.player1;
       playerScores.player2 = debugScores.player1;
-      console.log("Init: topCourtScore = " + topCourtScore + " btmCourtScore = " + btmCourtScore);
+      console.log(
+        "Init: topCourtScore = " +
+          topCourtScore +
+          " btmCourtScore = " +
+          btmCourtScore
+      );
     }
   }, []);
 
@@ -46,17 +66,30 @@ const GamePage = () => {
     console.log(courtPosition + " Match Point");
   };
 
-  const resetMatch = () => {
+  const resetGame = () => {
     if (DEBUG) {
       setTopCourtScore(debugScores.player1);
       setBtmCourtScore(debugScores.player1);
+      // setplayer1MatchScore(0);
+      // setplayer2MatchScore(0);
       playerScores.player1 = debugScores.player1;
       playerScores.player2 = debugScores.player1;
     } else {
       setTopCourtScore(0);
       setBtmCourtScore(0);
+      // setplayer1MatchScore(0);
+      // setplayer2MatchScore(0);
       playerScores.player1 = 0;
       playerScores.player2 = 0;
+    }
+  };
+  const resetMatches = () => {
+    if (DEBUG) {
+      setplayer1MatchScore(0);
+      setplayer2MatchScore(0);
+    } else {
+      setplayer1MatchScore(0);
+      setplayer2MatchScore(0);
     }
   };
 
@@ -64,14 +97,18 @@ const GamePage = () => {
     if (
       (topCourtScore === 21 && btmCourtScore < 20) ||
       (topCourtScore >= 20 &&
-       btmCourtScore >= 20 &&
-      (topCourtScore - btmCourtScore) >= 2) ||
-      (topCourtScore === 29)
+        btmCourtScore >= 20 &&
+        topCourtScore - btmCourtScore >= 2) ||
+      topCourtScore === 29
     ) {
       // Winner court is top court
-      console.log("Top Won with score of " + topCourtScore + " : " + btmCourtScore);
-
-      resetMatch();
+      console.log(
+        "Top Won with score of " + topCourtScore + " : " + btmCourtScore
+      );
+      setplayer1MatchScore((prevState) => prevState + 1);
+      playerMatchScores.player1++;
+      console.log("Match point to player" + " " + playerMatchScores.player1);
+      resetGame();
     }
   };
 
@@ -79,13 +116,17 @@ const GamePage = () => {
     if (
       (btmCourtScore === 21 && topCourtScore < 20) ||
       (btmCourtScore >= 20 &&
-       topCourtScore >= 20 &&
-      (btmCourtScore - topCourtScore) >= 2) ||
-      (btmCourtScore === 29)
+        topCourtScore >= 20 &&
+        btmCourtScore - topCourtScore >= 2) ||
+      btmCourtScore === 29
     ) {
       // Winner court is btm court
-      console.log("Btm Won with score of " + btmCourtScore + " : " + topCourtScore);
-      resetMatch();
+      console.log(
+        "Btm Won with score of " + btmCourtScore + " : " + topCourtScore
+      );
+      setplayer2MatchScore((prevState) => prevState + 1);
+      playerMatchScores.player2++;
+      resetGame();
     }
   };
 
@@ -93,8 +134,8 @@ const GamePage = () => {
     if (
       (topCourtScore === 20 && btmCourtScore <= 19) ||
       (btmCourtScore >= 20 &&
-       topCourtScore >= 20 &&
-      (topCourtScore - btmCourtScore) >= 1)
+        topCourtScore >= 20 &&
+        topCourtScore - btmCourtScore >= 1)
     ) {
       makeCourtGreen("top");
       return true;
@@ -102,8 +143,8 @@ const GamePage = () => {
     if (
       (btmCourtScore === 20 && topCourtScore <= 19) ||
       (btmCourtScore >= 20 &&
-       topCourtScore >= 20 &&
-      (btmCourtScore - topCourtScore) >= 1)
+        topCourtScore >= 20 &&
+        btmCourtScore - topCourtScore >= 1)
     ) {
       //Make Court green
       makeCourtGreen("btm");
@@ -126,8 +167,14 @@ const GamePage = () => {
     checkBtmWon(playerScores.player1, playerScores.player2);
   };
   const handleClickSwapCourt = () => {};
-  const handleResetMatch = () => {
-    resetMatch();
+  const handleResetGame = () => {
+    console.log("Pressed Reset Game");
+    resetGame();
+  };
+
+  const handleResetMatches = () => {
+    console.log("Pressed Reset Matches");
+    resetMatches();
   };
 
   return (
@@ -144,14 +191,26 @@ const GamePage = () => {
         playerScores={btmCourtScore}
         handleClickCourt={handleClickBtmCourt}
       />
-      <div className='gameScore-wrapper'>
-        <p>Scores</p>
-        <div className='gameScores'>
-          <GrPowerReset fontSize={50} onClick={handleResetMatch} />
+      <div className='score-wrapper'>
+        <div className='gameScore-wrapper'>
+          <p>Scores</p>
+          <div className='gameScores'>
+            <GrPowerReset fontSize={50} onClick={handleResetGame} />
 
-          <div className='courtScore'>{topCourtScore}</div>
-          <p>:</p>
-          <div className='courtScore'>{btmCourtScore}</div>
+            <div className='courtScore'>{topCourtScore}</div>
+            <p>:</p>
+            <div className='courtScore'>{btmCourtScore}</div>
+          </div>
+        </div>
+        <div className='gameMatch-wrapper'>
+          <p>Matches</p>
+          <div className='gameMatch'>
+            <GrPowerReset fontSize={50} onClick={handleResetMatches} />
+
+            <div className='courtMatchScore'>{player1MatchScore}</div>
+            <p>:</p>
+            <div className='courtMatchScore'>{player2MatchScore}</div>
+          </div>
         </div>
       </div>
     </div>
